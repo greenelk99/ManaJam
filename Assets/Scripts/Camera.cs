1952d91afa80943d79c6f0dccfaff494a9257124
azzinoth01 <markusdullnig@hotmail.com>
2024-03-22 21:37:12 +0100

    basic input

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private Vector3 _cammeraOffsetToPlayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = GlobalVariables.ActivePlayer.transform.position + _cammeraOffsetToPlayer;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour, Controlls.IPlayerMovementActions
+{
+
+
+    private Controlls _controller;
+
+    private Vector3 _moveDirection;
+
+    [SerializeField] private float _moveSpeed;
+
+    private Rigidbody _body;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        
+        Vector2 dir = context.ReadValue<Vector2>();
+
+        _moveDirection = new Vector3( dir.x,0,dir.y);
+
+    }
+
+    public void OnSwitch(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            enabled = !enabled;
+
+
+           if(enabled)
+            {
+
+                transform.position = GlobalVariables.ActivePlayer.transform.position;
+                GlobalVariables.ActivePlayer = this;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(_controller == null)
+        {
+            _controller = new Controlls();
+            _controller.Enable();
+            _controller.PlayerMovement.SetCallbacks(this);
+        }
+
+        _body = GetComponent<Rigidbody>();
+    }
+
+    public void Start()
+    {
+        GlobalVariables.ActivePlayer = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 _fallspeed = _body.velocity;
+        _fallspeed = new Vector3(0, _fallspeed.y, 0);
+        _body.velocity = _moveDirection * _moveSpeed + _fallspeed;
+        //transform.position = transform.position + _moveSpeed * _moveDirection * Time.deltaTime;
+
+    }
+}
